68bc10f8f1146562b4d20d179ce9848f4173d07e
unknown <kevinconwaylax@gmail.com>
2024-02-12 21:51:52 -0500

    start methods and response test method

--- bot.py
+++ bot.py
@@ -5,3 +5,7 @@ import requests
 # Add instance of discord client
 client = discord.Client()
 
+# Add on ready instance (when client is ready for use)
+@client.event
+async def on_ready():
+    print("We have logged in as " + client)
